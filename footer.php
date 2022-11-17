9ecd027343b3d1dff4f111bfecf6c3a6f0a8ec1d
Iva Bogicevic <ivabogicevic01@gmail.com>
2022-11-17 14:39:30 +0100

    pokusaj sredjivanja header-a i footer-a

--- footer.php
+++ footer.php
@@ -10,7 +10,11 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
+<div id="container">
+<div id="main">
 
+</div>
+</div>
 <li><a href="#link"><i class="fa fa-facebook"></i></a></li>
 <li><a href="#link"><i class="fa fa-instagram"></i></a></li>
 <li><a href="#link"><i class="fa fa-youtube"></i></a></li>
